a8aee66303de95dc3bd7b080bc4dfd91de6afa92
ekaone <ekaone3033@gmail.com>
2021-03-30 18:22:45 +0700

    update

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,12 @@ import React, { useEffect } from "react";
 
 function App() {
   useEffect(() => {
+    const env = import.meta.env.VITE_APP_TITLE;
     const options = {
-      // Required: API key
-      key: "29aATsius3WlYCfArDqx9oGjCEERUQia", // REPLACE WITH YOUR KEY !!!
+      key: env,
 
-      // Put additional console output
       verbose: true,
 
-      // Optional: Initial state of the map
-      // sambiloto -6.185725569944561, 106.87829266788778
       lat: -6.185725569944561,
       lon: 106.87829266788778,
       zoom: 10,
@@ -18,11 +15,7 @@ function App() {
 
     // Initialize Windy API
     windyInit(options, (windyAPI) => {
-      // windyAPI is ready, and contain 'map', 'store',
-      // 'picker' and other usefull stuff
-
       const { map } = windyAPI;
-      // .map is instance of Leaflet map
 
       L.popup()
         .setLatLng([-6.185725569944561, 106.87829266788778])
